1c2d15927cb0ed87b7e0188c76688e8f9ee63370
anushkajadhav21 <139699699+anushkajadhav21@users.noreply.github.com>
2023-10-15 15:19:39 +0530

    todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Todolist';
+  
+  newTask = '';
+
+  errorMessage = '';
+  todo: string[] = [];
+  
+  isEdit =false;
+  currentTaskIndex = 0;
+
+  add(newTask: string)
+  {
+    this.errorMessage = ''; // Clear any previous error message
+
+    if (newTask.trim() === '') {
+      this.errorMessage = 'Please enter the task.';
+      return; // Do not proceed if the task is empty
+    }else{
+      this.errorMessage = 'Please enter the task'
+    }
+
+    if (this.todo.includes(newTask)) {
+      this.errorMessage = 'Task already exists in the list.';
+      return; // for repeated Tasks
+    }
+
+    this.todo.push(newTask);
+    this.newTask = ''; 
+  }
+
+  remove(index: number)
+  {
+    this.todo.splice(index, 1);
+  }
+
+
+  edit(index: number)
+  {
+    this.isEdit = true;
+    this.currentTaskIndex = index;
+    this.newTask = this.todo[index];
+
+  }
+
+  save()
+  {
+    this.todo[this.currentTaskIndex] = this.newTask;
+    this.isEdit = false;
+    this.newTask = '';
+  }
+
+  cancelEdit(){
+    this.isEdit = false;
+    this.newTask = '';
+  }
 }
